7577c1900b8f60d000a27fb2d24cfa394362d591
fanyang <fanyang@smartx.com>
2024-02-27 17:04:42 +0800

    Move flags to standalone file

---
+++ cmd/flags.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"time"
+
+	"github.com/cockroachdb/errors"
+	"github.com/urfave/cli/v2"
+)
+
+var fileFlag = &cli.StringFlag{
+	Name:    "file",
+	Aliases: []string{"f"},
+	Action: func(c *cli.Context, s string) error {
+		ok, err := fileExists(s)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return errors.Newf("file %v not exists", s)
+		}
+		return nil
+	},
+}
+
+var prefixFlag = &cli.StringFlag{
+	Name:    "prefix",
+	Aliases: []string{"p"},
+	Action: func(c *cli.Context, s string) error {
+		if s == "" {
+			return errors.New("prefix is empty")
+		}
+		return nil
+	},
+}
+
+var sessionTimeoutFlag = &cli.DurationFlag{
+	Name: "session-timeout", Value: 10 * time.Second,
+}
+
+var overwriteFlag = &cli.BoolFlag{
+	Name:  "overwrite",
+	Value: false,
+}
+
+var hostsFlag = &cli.StringFlag{
+	Name: "hosts",
+}
+
+var clearFlag = &cli.BoolFlag{
+	Name:    "clear",
+	Aliases: []string{"C"},
+	Value:   false,
+}
--- cmd/import.go
+++ cmd/import.go
@@ -8,7 +8,6 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
-	"time"
 
 	"github.com/cockroachdb/errors"
 	"github.com/go-zookeeper/zk"
@@ -28,51 +27,6 @@ func fileExists(p string) (bool, error) {
 		return false, err
 	}
 	return true, nil
-}
-
-var fileFlag = &cli.StringFlag{
-	Name:    "file",
-	Aliases: []string{"f"},
-	Action: func(c *cli.Context, s string) error {
-		ok, err := fileExists(s)
-		if err != nil {
-			return err
-		}
-		if !ok {
-			return errors.Newf("file %v not exists", s)
-		}
-		return nil
-	},
-}
-
-var prefixFlag = &cli.StringFlag{
-	Name:    "prefix",
-	Aliases: []string{"p"},
-	Action: func(c *cli.Context, s string) error {
-		if s == "" {
-			return errors.New("prefix is empty")
-		}
-		return nil
-	},
-}
-
-var sessionTimeoutFlag = &cli.DurationFlag{
-	Name: "session-timeout", Value: 10 * time.Second,
-}
-
-var overwriteFlag = &cli.BoolFlag{
-	Name:  "overwrite",
-	Value: false,
-}
-
-var hostsFlag = &cli.StringFlag{
-	Name: "hosts",
-}
-
-var clearFlag = &cli.BoolFlag{
-	Name:    "clear",
-	Aliases: []string{"C"},
-	Value:   false,
 }
 
 func getDepth(key string) int {
